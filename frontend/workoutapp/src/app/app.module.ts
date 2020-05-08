745a7385625c35fafdec3304bf48c446f99f34cf
Siedlar <ksiedlarski@gmail.com>
2020-05-08 13:31:23 +0200

    Dodanie nawigacji menu

--- frontend/workoutapp/src/app/app.module.ts
+++ frontend/workoutapp/src/app/app.module.ts
@@ -13,7 +13,9 @@ import { Routes, RouterModule } from '@angular/router'
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', component: WelcomepageComponent },
+  { path: '', component: WelcomepageComponent
+ },
+
   { path: 'login', component: LoginComponent },
 ]
 @NgModule({
